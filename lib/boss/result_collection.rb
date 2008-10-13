38f3b345c21a3afe2bc71fdc06e3b691ac6d2ec3
Jared Pace <jared@codewordstudios.com>
2008-10-13 17:52:09 -0400

    added attributes to result collection to allow it to work with will_paginate helper out of the box.

--- lib/boss/result_collection.rb
+++ lib/boss/result_collection.rb
@@ -24,6 +24,32 @@ module Boss
     def [](key)
       @results[key]
     end
+    
+    # Implements neccessary api for the will_paginate view helper
+    # to work with result sets out of the box
+    def size
+      @results.size
+    end
+    
+    def empty?
+      @results.empty?
+    end
+    
+    def previous_page
+      self.current_page == 1 ? nil : self.current_page.to_i-1
+    end
+    
+    def next_page
+      self.current_page == self.total_pages ? nil : self.current_page+1
+    end
+    
+    def total_pages
+      (self.totalhits.to_f/self.page_count.to_f).ceil
+    end
+    
+    def current_page
+      (self.start.to_i/self.page_count.to_i) + 1
+    end
 
   end
 end
